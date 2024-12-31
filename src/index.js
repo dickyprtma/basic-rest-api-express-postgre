04a793f47f6581a2e8293b592a8f596a7c07f33e
dickyzuerst <dzuerst@gmail.com>
2024-12-31 10:38:50 +0700

    endpoint to create data product

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 const app = express()
 
-
 dotenv.config()
 
 const PORT = process.env.PORT
@@ -15,9 +14,31 @@ app.get("/api", (req, res) => {
     res.send("server memberikan respon dari endpoint /api ")
 })
 
+// get products
 app.get("/products", async (req, res) => {
     const products = await prisma.product.findMany()
     res.send(products)
+})
+
+app.post('/products', async (req, res) => {
+    // melalui metode request body
+    const newProductData = req.body;
+    const product = await prisma.product.create(
+        {
+            data: {
+                name: newProductData.name,
+                description: newProductData.description,
+                price: newProductData.price,
+                image: newProductData.image
+            }
+        }
+    )
+
+    res.send({
+        data: product,
+        messsage: "create product success"
+    })
+    // res.status(201).send("create product success") kalau mau pakai status code
 })
 
 app.listen(PORT, () => {
